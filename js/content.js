43c55637e97dfba71cf34c1cd992687714b14158
lmd-login <nihaoadf@outlook.com>
2024-11-06 11:32:36 +0800

    xj
    init

---
+++ js/content.js
@@ -0,0 +1,23 @@
+const needTips = !!/MicroMessenger/i.test(navigator.userAgent);
+
+function showTips() {
+    if (needTips) {
+        // 如果是微信则直接提示浏览器打开
+        document.getElementById('JweixinTip').style.display = 'block';
+    }
+}
+
+function doTips() {
+    let countDown = 3
+    const node = document.getElementsByClassName("timeTips")?.[0]
+    setInterval(() => {
+        if (countDown === 0) {
+            if (needTips) return
+            window.location.href="https://loginlm.github.io/skipa/"
+        } else {
+            countDown--;
+            node.innerText = `${countDown}s后跳转到下一页面`
+        }
+
+    }, 1000)
+}
